905d9751b833e05cc26d1ca92e83a1663756d55f
Kore Rakesh <korerakesh2004@gmail.com>
2025-09-12 21:42:44 +0530

    Update Recharge.java

--- src/main/java/com/webcont/lumen_project_1/model/Recharge.java
+++ src/main/java/com/webcont/lumen_project_1/model/Recharge.java
@@ -7,7 +7,6 @@ import java.time.LocalDateTime;
 @Entity
 @Table(name = "recharges")
 public class Recharge {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
